248994b126e936d03677385627656f1c098ec4b1
saqibmurtaza <saqibmurtazakhan@gmail.com>
2024-04-06 09:40:29 +0500

    Assertion error in Test update & Delete Todo

---
+++ tests/test_main.py
@@ -0,0 +1,54 @@
+from fastapi.testclient import TestClient
+from fastapi_project0.main import app, Todo, settings, get_session
+from sqlmodel import create_engine, Session, SQLModel
+
+######### TEST 1 : READ ROOT
+def test_read_root_todo():
+    client = TestClient(app=app)
+    gen_request_to_endpoint = client.get("/")
+    assert gen_request_to_endpoint.status_code == 200
+    assert gen_request_to_endpoint.json() == {"Hello": "My first API Endpoint"}
+
+########## TEST 2 : POST TODO 
+def test_post_todo():
+    client = TestClient(app=app)
+    todo_content = {"content": "buy eggs"}
+    gen_request_to_endpoint = client.post("/todos/", json=todo_content)
+    data = gen_request_to_endpoint.json()
+    assert gen_request_to_endpoint.status_code == 200
+    assert data["content"] == todo_content["content"]
+
+########### TEST 3: READ TODO LIST
+def test_read_todos_list():
+    client = TestClient(app=app)
+    gen_request_to_endpoint = client.get("/todos/")
+    assert gen_request_to_endpoint.status_code == 200
+    assert isinstance(gen_request_to_endpoint.json(), list)
+
+########### TEST 4 : UPDATE
+def test_update_todo():
+    client = TestClient(app=app)
+    add_test_todo_data = {"content":"Buy Shoes"}
+    gen_req_to_add_test_todo = client.post("/todos/", json=add_test_todo_data)
+    assert gen_req_to_add_test_todo.status_code == 200
+    created_todo_id = gen_req_to_add_test_todo.json()["id"]
+    assert created_todo_id is not None, "Failed to extract id"
+    updated_test_todo_data = {"content":"Buy Bata Shoes"}
+    gen_req_to_update_todo = client.put(f"/todos/{created_todo_id}", json=updated_test_todo_data)
+    updated_data = gen_req_to_update_todo.json()
+    # assert updated_data["content"] == updated_test_todo_data["content"]
+    
+
+
+
+########## TEST 5: DELETE
+def test_delete_todo():
+    client = TestClient(app=app)
+    todo_content = {"content": "buy eggs"}
+    gen_request_to_add_todo = client.post("/todos/", json=todo_content)
+    assert gen_request_to_add_todo.status_code == 200
+    created_todo_id = gen_request_to_add_todo.json()["id"]
+    gen_request_to_delete_todo = client.delete(f"/todos/{created_todo_id}")
+
+    assert gen_request_to_delete_todo.status_code == 200
+    # assert gen_request_to_delete_todo.json() == {"message": "Todo deleted successfully"}
